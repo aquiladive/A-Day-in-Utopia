20578cdb7cb8ad9325a2d9447a3a1a53eeb12aa7
aquiladive <102308614+aquiladive@users.noreply.github.com>
2023-02-23 12:04:47 +0530

    Update Classes.cpp
    added two more items gainable in Ch1 pt2b

--- Classes.cpp
+++ Classes.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <ctime>
-#include <cstdlib> //ctime and cstdlib used for the srand(time(0)) and rand(), which are used in Monster's attack()
+#include <cstdlib> //ctime and cstdlib used for the srand(time(0)) and rand())
 using namespace std;
 
 class Character {
@@ -23,14 +23,13 @@ class Character {
     virtual int attack()=0;
 };
 
-class NPC: private Character {
+class NPC: public Character {
     using Character::Character; //copies the constructor from Character
     int attack() {
         return ATK;
     }
     //using NPC instead of directly using Character because as it has a virtual function, direct objects are not allowed
     //the virtual function is important because it's defined differently for Protag and for Monster
-    //private because NPC details don't need to be changed by anything in the program
 };
 
 class Protag: public Character {
@@ -38,8 +37,89 @@ class Protag: public Character {
     public:
     int Level=1;
     int battleExp=0;
+    int Beyonder[2];
+    string Path;
+    
     int attack() {
         return ATK;
+    }
+
+    int beyonder(int belief) {
+        srand(time(0));
+        if(belief==1) {
+            //evernight
+            int pathway=rand()%3+1;
+            switch(pathway) {
+                case 1: Beyonder[0]=1;
+                Path="Sleepless";
+                break;
+                case 2: Beyonder[0]=2;
+                Path="Corpse Collector";
+                break;
+                case 3: Beyonder[0]=3;
+                Path="Warrior"; //find out the name of Combat sequence 9
+                break;
+            }
+        }
+        else if(belief==2) {
+            //storm
+            Beyonder[0]=4;
+            Path="Sailor";
+        }
+        else if(belief==3) {
+            //lilith
+            int pathway=rand()%2+1;
+            switch(pathway) {
+                case 1: Beyonder[0]=5;
+                Path="Apothecary";
+                break;
+                case 2: Beyonder[0]=6;
+                Path=""; //find out mother pathway
+                break;
+            }
+        }
+        else if(belief==4) {
+            //steam
+            int pathway=rand()%2+1;
+            switch(pathway) {
+                case 1: Beyonder[0]=7;
+                Path="Savant";
+                break;
+                case 2: Beyonder[0]=8;
+                Path="Mystery Pryer";
+                break;
+            }
+        }
+        else if(belief==5) {
+            //fool
+            int pathway=rand()%3+1;
+            switch(pathway) {
+                case 1: Beyonder[0]=9;
+                Path="Seer";
+                break;
+                case 2: Beyonder[0]=10;
+                Path="Marauder";
+                break;
+                case 3: Beyonder[0]=11;
+                Path="Apprentice";
+                break;
+            }
+        }
+        else {
+            //atheist
+            int pathway=rand()%3+1;
+            switch(pathway) {
+                case 1: Beyonder[0]=12;
+                Path="Bard"; //sun sequence 9
+                break;
+                case 2: Beyonder[0]=13;
+                Path="Psychologist";
+                break;
+                case 3: Beyonder[0]=14;
+                Path="Shepherd"; //hanged man sequence 9
+                break;
+            }
+        }
     }
 };
 
@@ -110,7 +190,9 @@ NPC Lana("Lana",40,7,7,9,9,5);
 //--
 
 Monster Bounceshroom("Bounceshroom",6,9,1,10,5,0);
+Monster Slime("Slime",10,7,4,8,6,0);
 Monster DarkWolf("Dark Wolf",12,15,4,10,3,2);
+Monster MetalCube("Metal Cube",10,8,10,6,10,0);
 
 //--
 
@@ -120,5 +202,7 @@ int inventoryCount;
 Item bottle("bottle of essential scent","A gift from the second mate. It contains a mixture that can soothe seasickness on inhaling.",1);
 Item emblem("Lana's emblem","Given to you by Lana. It can be used by squeezing it and shouting \"Light\".",5);
 int emblemUse; //tracks the breaking point of the emblem
+Item slime("slime remnant","It's a greyish spherical object that has a solid feel, surprising compared to the slimes themselves.",1);
+Item map("map", "A sketch made by yourself of Utopia and the path you've taken through it.",0);
 
 //--
